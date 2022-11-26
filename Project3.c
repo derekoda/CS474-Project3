ba90da3af2d500a060e65520591ca0f2c6961898
sball38 <sball38@nmsu.edu>
2022-11-26 10:56:10 -0700

    implemented threads and start fo computations

--- Project3.c
+++ Project3.c
@@ -26,10 +26,25 @@
 #include <time.h>
 #include <math.h>
 
+pthread_mutex_t mutexBuffer;
+
+// data to be passes with each thread 
+// and to store results
+typedef struct thread_data {
+    float* mat;
+    int rstart;
+    int cstart;
+    int r;
+    int c;
+    float result;
+} thread_data;
+
 // bestRC will find the best multiple to generate our matrix
 // ex if T = 6, R>=C, R = N/C, so R = 3, C = 2
 int bestRC(int n)
 {
+    if(n==1)
+        return 1;
     // Initialize the maximum prime factor
     // variable with the lowest one
     int maxPrime = -1;
@@ -58,10 +73,35 @@ int bestRC(int n)
     return maxPrime;
 }// end bestRC
 
+// rowSum will calculate the row sum of the passed section of the 
+// matrix, it will be called with a section of the matrix by each thread
+void* rowSum(void* args)
+{
+    thread_data *tdata=(thread_data *)args;
+    int rstart = tdata->rstart;
+    int cstart = tdata->cstart;
+    int r = tdata->r;
+    int c = tdata->c;
+    float *mat = tdata->mat;
+    float res = tdata->result;
+
+    //printf("rstart = %d\n cstart = %d\n r = %d\n c = %d\n",rstart,cstart,r,c);
+    for (int b = rstart; b < rstart+r; b++)
+    {
+        for(int a = cstart; a < cstart+c; a++)
+        {
+            res = res + mat[a];
+            printf("%f\n%f\n", mat[a],res);
+        }// end for
+    }//end for
+
+}// end rowSum
 
 int main(int argc, char *argv[]) {
 
+    // rows per thread
     int R;
+    // columns per thread
     int C;
     // i is a counter variable
     int i = 1;
@@ -90,6 +130,8 @@ int main(int argc, char *argv[]) {
 
     R = bestRC(T);
     C = T/R;
+    R = N/R;
+    C = N/C;
     // used to make sure R >= C
     if(C > R)
     {
@@ -114,9 +156,51 @@ int main(int argc, char *argv[]) {
         for(int j = 0; j < N; j++)
         {
             matrix[i][j] = V;
+            //printf("%f\n", V);
+            //printf("%f\n\n", matrix[i][j]);
         }// end for
     }// end for
-    
-    printf("%f\n", V);
+
+    // threads
+	pthread_t th[T+1];
+	pthread_mutex_init(&mutexBuffer, NULL);
+
+	// create threads
+	for(int t = 1; t <= T; t++)
+	{
+        // create parameters in struct 
+        thread_data tdat;
+        tdat.rstart = (t-1)*R;
+        tdat.cstart = (t-1)*C;
+        tdat.r = R;
+        tdat.c = C;
+        tdat.mat = *matrix;
+        tdat.result = 0;
+        //printf("%f\n", matrix[0][0]);
+         
+        
+		// thread is not init
+		if(pthread_create(&th[t], NULL, &rowSum, (void*)&tdat) != 0)
+		{
+			perror("error creating producer thread");
+		}
+	}// end for creating threads
+
+	// join the threads
+	for(int i = 1; i <= T; i++)
+	{
+		// threads were not joined
+		if(pthread_join(th[i], NULL) != 0)
+		{
+			perror("error joining threads");
+		}
+	}// end for joining threads
+
+
+	// destroy threads and semaphores
+	pthread_mutex_destroy(&mutexBuffer);
+
+	// end
+	printf("\nEnd of program\n");
 }
 
